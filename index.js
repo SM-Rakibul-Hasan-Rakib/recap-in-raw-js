37abe2151ee6a49e9f875bcda0f8521007dbfeb5
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-03-08 14:54:44 +0600

    update

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ const student = {
 const about = `My Name is ${student.name} age of ${student.age} has number ${numbers[2]} also liked movies ${student.movies[0]}`;
 
 console.log(about);
+
+
+
+const marks = [89, 35, 98, 12];
+const students = {
+  name: 'Sakib Khan',
+  age: 32,
+  movies: ['sakib khan', 'Dunki']
+};
+
+const abouts = `My marks number is  ${students.age}`
+
+console.log(abouts);
+
+// arrow function 
+
+const getFiftyFive = () => 55;
+const addSixtyFive = num => num + 65;
+const isEven = x => x % 2 ==  0;
+const addThree = (x, y, z) => x + y + z;
+const doMath = (num1 , num2)
+
+// console.log(getFiftyFive, addThree, addSixtyFive,isEven);
